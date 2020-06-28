d3d8c37607e4f8f5040f1e67a78f8959fc802963
mithlars <a@mith.pro>
2020-06-28 03:08:59 +0300

    0

--- ContactBook.py
+++ ContactBook.py
@@ -1,13 +1,41 @@
 import sqlite3
-datafile = input("Введите адрес файла базы данных")
+# Необходимо создать модуль с классом работы с базой данных
+# Необходио заключить все это в блок try и предусмотреть ошибки
+# Необходим графический интерфейс для ввода адреса и имени файла базы данных
+# Необходимо определять в какой операционной системе осуществляется запуск программы и,
+# исходя из этого, формировать адреса файлов конфигурации и базы данных
+
+datafile = 'ContactBook.db'
 conn = sqlite3.connect(f'{datafile}')
 c = conn.cursor()
 
-tables = c.execute("CREATE TABLE favourite")
+c.execute("SELECT name FROM sqlite_master WHERE type = 'table';")
+tables1 = c.fetchall()
+tables = []
+for i in range(0,len(tables1)):
+    tables.append(tables1[i][0])
+    i += 1
 
+if "favourites" not in tables:
+    print("Create the table 'favourites'")
+    c.execute("CREATE TABLE favourites (ContactID int);")
+else:
+    print("The table 'favourites' already made")
 
-
-
+if "contacts" not in tables:
+    print("Create the table 'contacts'")
+    c.execute("CREATE TABLE contacts ("
+              "ContactID int, "
+              "nickname varchar(50), "
+              "surname varchar(20), "
+              "name varchar(20), "
+              "otchestvo varchar(20), "
+              "comment varchar(300), "
+              "listNumberIDs varchar, "
+              "listAdresIDs varchar"
+              ");")
+else:
+    print("The table 'contacts' already made")
 
 
 
